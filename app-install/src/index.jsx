9db48cbd7a3451d8aca31d7d55cc4a048f978a97
RicardoEspindol <ricardoespindola128@gmail.com>
2023-03-08 21:52:21 -0300

    version1

---
+++ app-install/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Timer from "./components/exemplo1";
+import ReactDOM from "react-dom/client";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Timer></Timer>)
