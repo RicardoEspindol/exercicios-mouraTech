1a4cc8c334f4b00407b860622a420fd6a19702e0
RicardoEspindol <ricardoespindola128@gmail.com>
2023-03-09 21:41:13 -0300

    cronometro

--- app-install/src/components/exemplo1.jsx
+++ app-install/src/components/exemplo1.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ReactDOM from "react";
+import "../index.css"
 
 
 class Timer extends React.Component{
 
     constructor(props){
         super(props);
-        this.state =  {seconds: 0}
+        this.state =  {seconds: this.props.start}
     }
-
     tick() {
         this.setState(state => ({
           seconds: state.seconds + 1
         }));
       }
+      startStop(){
+        console.log("clicou no botao")
+      }
     
       componentDidMount() {
-        this.interval = setInterval(() => this.tick(), 1000);
+        this.interval = setInterval(() => this.tick(), this.props.ms );
       }
     
       componentWillUnmount() {
@@ -25,9 +28,14 @@ class Timer extends React.Component{
     
       render() {
         return (
-          <div>
-            <h1>Segundos: {this.state.seconds}</h1>
-          </div>
+          <>
+            <div className="bg-azulEscuro border-2 border-yellow-600 w-96 h-16 mb-4  mt-4 rounded-xl flex flex-col items-center justify-center m-auto">
+              <h1 className="text-white text-5xl cronometro">Segundos: {this.state.seconds}</h1>
+            </div>
+            <button 
+            onClick={() => this.startStop()}
+            className="bg-azulEscuro text-white m-auto flex px-4 py-4 rounded-xl">Iniciar/Parar</button>
+          </>
         );
       }
     
--- app-install/src/index.jsx
+++ app-install/src/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Timer from "./components/exemplo1";
 import ReactDOM from "react-dom/client";
+import "./index.css"
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer></Timer>)
+root.render(
+    <>
+        <Timer start={0} ms={1000}></Timer>
+    </>
+)
