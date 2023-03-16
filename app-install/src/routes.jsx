f6799229d4a356ce685149c80bf2db5df31a88fa
RicardoEspindol <ricardoespindola128@gmail.com>
2023-03-15 22:36:02 -0300

    multistep form

--- app-install/src/routes.jsx
+++ app-install/src/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Exemplo1 from './pages/Exemplo1'
-import Menu from './components/Menu'
+import Multistep from './pages/Multistep';
 
 
 
@@ -10,6 +10,8 @@ const routes = [
   { path: '/', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
   { path: '/exemplo1', element: <Exemplo1 /> },
+  { path: '/multistep', element: <Multistep /> },
+
 
 ];
 export default routes;
